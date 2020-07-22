206bc8112c0cf96c07d9721219727db446e80c08
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-07-22 10:24:53 +0530

    spike on pug

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const app = require('./src/app.js');
 
 const PORT = process.env.PORT || 8000;
+
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './templates');
+
+app.get('/name', (req, res) => {
+  res.render('name', { name: 'John' });
+});
+
 app.listen(PORT, () => console.log('server is listening', PORT));

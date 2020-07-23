3caa33b8b61e7a7d89af55dab72b37a69f23e708
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-07-23 16:14:06 +0530

    |#4|venky/anuja| fixed failing tests

--- src/app.js
+++ src/app.js
@@ -3,12 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const cookieParser = require('cookie-parser');
-const db = new sqlite3.Database(app.locals.db_path);
 
 const { isSignedIn, publish } = require('./handlers');
-const Database = require('./database');
 
-app.locals.db = Database(db);
 app.locals.sessions = {};
 
 app.use(morgan('dev'));

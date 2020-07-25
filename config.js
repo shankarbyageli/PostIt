3cdbf33486b83490a01a64c955fe49e2c6cf037b
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-07-25 10:31:12 +0530

    |#4|venky/anuja| remvoed linter error

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   db: process.env.db,
-  client_id: '547eef445f7646f74917',
-  client_secret: '66ee95a4880d68a5087eeea7d29ebe27c64ab9c2',
+  clientId: '547eef445f7646f74917',
+  clientSecret: '66ee95a4880d68a5087eeea7d29ebe27c64ab9c2',
 };
-

fdbfa7c7b00652199425b0d27d48264df8519e26
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-07-28 18:08:21 +0530

    |#11|Phaneendra/Anuja| Made empty comment non-publishable

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,17 +1,32 @@
 const publishComment = async function (blogId) {
   const newComment = document.getElementById('new-comment').innerText;
-  await fetch('/user/publishComment', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      comment: newComment,
-      blogId,
-    }),
-  }).then((data) => {
-    if (data.status == 200) {
-      window.location.href = `/seeAllComments/${blogId}`;
-    }
-  });
+  if (newComment.trim()) {
+    await fetch('/user/publishComment', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        comment: newComment,
+        blogId,
+      }),
+    }).then((data) => {
+      if (data.status == 200) {
+        window.location.href = `/seeAllComments/${blogId}`;
+      }
+    });
+  }
+};
+
+const isAbleToPublish = function () {
+  const newComment = document.getElementById('new-comment').innerText;
+  const publishBtn = document.querySelector('#publish');
+  if (newComment.trim()) {
+    publishBtn.setAttribute(
+      'style',
+      'background-color: #03a87c;cursor: pointer'
+    );
+    return;
+  }
+  publishBtn.setAttribute('style', 'background-color: #c5cac9');
 };

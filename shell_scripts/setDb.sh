5c0bb1ad87e8324b40f37d1ffd87f64f4abc7de8
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-08-05 09:50:08 +0530

    #26|Anuja/Venky|added svg for profile-picture in editprofile.pug

--- shell_scripts/setDb.sh
+++ shell_scripts/setDb.sh
@@ -2,10 +2,4 @@
 
 sqlite3 $1 << END_SQL
 
-.read sqlScripts/createTables.sql
-.mode csv
-.separator '|'
-.import "./data/usersData.csv" users
-.import "./data/followersData.csv" followers
-
 END_SQL

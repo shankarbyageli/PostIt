22eb577d1a72f713ec499ea41f77880f494f149b
Shankara <=>
2020-08-06 16:26:28 +0530

    |#10|shankar/phaneendra|modified config.js
    Removed client_id and client_secret
    Using process.env to get client_id and client_secret

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   db: process.env.db,
-  clientId: '547eef445f7646f74917',
-  clientSecret: '66ee95a4880d68a5087eeea7d29ebe27c64ab9c2',
+  clientId: process.env.client_id,
+  clientSecret: process.env.client_secret
 };

25fc1034c943c45852008a161cbcec56b7b43313
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-08-12 14:14:45 +0530

    |#31|Anuja/phani| modified the tests

--- test/testDatabase.js
+++ test/testDatabase.js
@@ -138,24 +138,6 @@ describe('getPost', () => {
 });
 
 describe('getUsersPosts', () => {
-  it('should give error if database failure', (done) => {
-    const orderBy = sinon.stub().rejects('error');
-    const where = sinon.stub().returns({ orderBy });
-    const join = sinon.stub().returns({ where });
-    const select = sinon.stub().returns({ join });
-    const newDb = sinon.stub().returns({ select });
-
-    const database = new Database(null, newDb);
-    database.getUsersPosts(1, 1).then((actual) => {
-      assert.deepStrictEqual(actual, [{}, {}]);
-      done();
-    }, null);
-    database.getUsersPosts(1, 1).then(null, (actual) => {
-      assert.equal(actual, 'error');
-      done();
-    });
-  });
-
   it('should get all the posts of given type from database', (done) => {
     const orderBy = sinon.stub().resolves([{}, {}]);
     const where = sinon.stub().returns({ orderBy });
